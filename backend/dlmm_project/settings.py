dfc3cd0a82c9285a426fb9b49048605450ef4bfe
Cyril Barratier <cymyster@gmail.com>
2025-09-19 13:00:13 +0200

    Fix template directory in settings

--- backend/dlmm_project/settings.py
+++ backend/dlmm_project/settings.py
@@ -50,10 +50,11 @@ ROOT_URLCONF = 'dlmm_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / '../../frontend/build'], # Chemin vers les fichiers de la build React
+        'DIRS': [BASE_DIR / 'templates'], # Ajoutez cette ligne
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
